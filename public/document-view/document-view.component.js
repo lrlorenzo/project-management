a94238981db3391f809b35953aed5bd82e074556
c.marcaida <cymon.marcaida12@gmail.com>
2016-07-20 16:50:02 +0800

    Added pdfkit init

--- public/document-view/document-view.component.js
+++ public/document-view/document-view.component.js
@@ -5,6 +5,11 @@ angular.
     controller: ['$scope', '$http', '$location',
       function documentViewController($scope, $http, $location) {
 		  var self = this;
+		  var doc = new pdfkit();
+		  var stream = doc.pipe(blobStream());
+
+		  doc.fontSize(25)
+   			.text('Here is some vector graphics...', 100, 80);
 		  console.log('view document here');
 		  //console.log("routeParams:" + $routeParams.projectId);
 		  $http.get('/view').then(function(response) {
