ff05399418c5549e467bc8b580a90a0cd8ca8cfd
c.marcaida <cymon.marcaida12@gmail.com>
2016-07-21 15:23:08 +0800

    Added View PDF Document feature (WIP)

--- routes/view.js
+++ routes/view.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
-var PDFDocument = require('pdfkit');
+var mysql = require('mysql');
 var blobStream = require('blob-stream');
 var fs = require('fs');
+var PDFDocument = require('pdfkit');
 
-
+var id = "153629";
 
 // var mysql = require('mysql');
 /*
@@ -13,16 +13,17 @@ var bodyParser = require('body-parser');
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 */
-// function createConn() {
-//     var connection = mysql.createConnection({
-//         user: 'training',
-//         password: 'training',
-//         host: '10.164.37.53',
-//         port: 3306,
-//         database: 'training'
-//     });
-//     return connection;
-// }
+
+function createConn() {
+	var connection = mysql.createConnection({
+		user: 'training',
+		password: 'training',
+		host: '10.164.37.53',
+		port: 3306,
+		database: 'training'
+	});
+	return connection;
+}
 
 /*
 function Project(id, name) {
@@ -32,19 +33,31 @@ function Project(id, name) {
 */
 
 router.get('/', function(req, res, next) {
-  res.send("Hello GET view");
+	console.log('yyyyyyyyy init');
+	
+	var objConn = createConn();
+	objConn.query('SELECT * FROM basic_information where empid = ? AND active_flag = ?', ['120698', 'Y'], function(error, rows) {
+		console.log('Basic Information Data');
+		console.log(rows.length);
+		
+		var doc = new PDFDocument();
+		//doc.pipe(fs.createWriteStream('file.pdf'));
+		doc.text(rows[0].FIRSTNAME + ' ' + rows[0].MIDDLENAME + ' ' + rows[0].LASTNAME ,100,100);
+		doc.text('WeServ Systems International, Inc.',100,100);
+		doc.pipe(res);
+		doc.end();
+  });
 });
 
 router.get('/:id', function(req, res, next) {
 	var doc = new PDFDocument();
-	doc.pipe(fs.createWriteStream('file.pdf'));
+	//doc.pipe(fs.createWriteStream('file.pdf'));
 	doc.text('Hello World and World again!!',100,100);
 	doc.pipe(res);
 	doc.end();
 });
 
 router.post('/:id', function(req, res) {
- 
   res.send("Hello POST view");
 });
 
